9779e312e297b884db92698283ed68d85578d9f9
KimTaeyoun <kimty1121@gmail.com>
2021-06-07 00:29:36 +0900

    prop test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 
-function Food({fav}) {
-  return <h1>I like {fav}</h1>;
+function Food({name}) {
+  return <h1>I like {name}</h1>;
 }
 
 const foodIlike = [
-  {name:"kimchi"},
-  {name:"pizza"},
-  {name:"chicken"},
-  {name:"rice"},
-  {name:"bread"}
+  {id:1, name:"kimchi"},
+  {id:2, name:"pizza"},
+  {id:3, name:"chicken"},
+  {id:4, name:"rice"},
+  {id:5, name:"bread"}
 ];
 
 function App(props) {
   return (
     <div>
-      {foodIlike.map(dish => <Food fav={dish.name}/> )}
+      {foodIlike.map(dish => (
+        <Food key={dish.id} name={dish.name}/> 
+      ))}
     </div>
   );
 }
