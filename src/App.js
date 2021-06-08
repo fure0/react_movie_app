dd183d55a79869e6f585b02960f46bcc16919c2b
KimTaeyoun <kimty1121@gmail.com>
2021-06-09 00:10:50 +0900

    component test

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Food({name, rating}) {
-  return (
-    <div>
-      <h1>I like {name}</h1>
-      <h4>{rating}/5.0</h4>
-    </div>
-  );
-}
-
-// type check
-// push test
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
-
-const foodIlike = [
-  {id:1, name:"kimchi", rating: 5},
-  {id:2, name:"pizza", rating: 3},
-  {id:3, name:"chicken", rating: 5},
-  {id:4, name:"rice", rating: 5},
-  {id:5, name:"bread", rating: 2}
-];
-
-function App(props) {
-  return (
-    <div>
-      {foodIlike.map(dish => (
-        <Food key={dish.id} name={dish.name} rating={dish.rating}/> 
-      ))}
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
+  render() {
+    return  (
+      <div>
+        <h1>I am a Class {this.state.count}</h1>
+        <button onClick={this.add}>add</button>
+        <button onClick={this.minus}>minus</button>
+      </div>
+    )
+  };
 }
 
 export default App;
